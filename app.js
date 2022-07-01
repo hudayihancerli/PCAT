6fd8f9768e9d5881a7ac1be87b73adfd44032ab1
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-07-01 18:23:21 +0300

    template engine

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const path = require('path')
+const ejs = require('ejs');
 
 const app = express();
 
-
+//templae engine
+app.set("view engine", "ejs")
 
 //middelewares
 app.use(express.static('public'))
 
+//routes
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    res.render('index')
+})
+//routes
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+//routes
+app.get('/add', (req, res) => {
+    res.render('add')
 })
 
 
