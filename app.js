6f33862970fab325fa0762b4cb9f4897611d8009
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-07-03 01:38:12 +0300

    create database, photo schema, photo list

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path')
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect Db
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 
 //templae engine
 app.set("view engine", "ejs")
@@ -10,9 +19,16 @@ app.set("view engine", "ejs")
 //middelewares
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended: true })) // url'deki datayı okuyor
+
+app.use(express.json()) // url'deki datayı json'a çeviriyor
+
 //routes
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({})
+    res.render('index', {
+        photos
+    })
 })
 //routes
 app.get('/about', (req, res) => {
@@ -22,7 +38,11 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
     res.render('add')
 })
-
+//routes
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect('/')//index'e yönlendir
+});
 
 
 const port = 1000;
