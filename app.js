c6489ec8a5e712219ffdefa2a28ee7189df161c2
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-07-03 22:35:50 +0300

    upload photo

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload')
 const path = require('path')
 const ejs = require('ejs');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -17,14 +19,13 @@ app.set("view engine", "ejs")
 
 //middelewares
 app.use(express.static('public'))
-
 app.use(express.urlencoded({ extended: true })) // url'deki datayı okuyor
-
 app.use(express.json()) // url'deki datayı json'a çeviriyor
+app.use(fileUpload())
 
 //routes
 app.get('/', async (req, res) => {
-    const photos = await Photo.find({})
+    const photos = await Photo.find({}).sort('-dateCreated')
     res.render('index', {
         photos
     })
@@ -33,22 +34,38 @@ app.get('/', async (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about')
 })
+
 // id parametre
 app.get('/photos/:id', async (req, res) => {
-    // console.log(req.params.id)
     const photo = await Photo.findById(req.params.id);
     res.render('photo', {
         photo
     })
 })
-//routes
 app.get('/add', (req, res) => {
     res.render('add')
 })
-//routes
 app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/')//index'e yönlendir
+    // console.log(req.files.image)
+    // await Photo.create(req.body)
+    // res.redirect('/')//index'e yönlendir
+
+    const uploadDir = 'public/uploads'
+
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir)
+    }
+
+    let uploadeImage = req.files.image
+    let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name
+
+    uploadeImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadeImage.name
+        })
+        res.redirect('/');
+    })
 });
 
 
